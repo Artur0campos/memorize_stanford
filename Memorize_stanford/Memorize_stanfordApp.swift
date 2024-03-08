68b778d2c83db356455b227c398f9f29e18ba498
Artur0campos <arturcamposcamara@gmail.com>
2024-03-08 14:33:05 -0300

    Initial Commit

---
+++ Memorize_stanford/Memorize_stanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_stanfordApp.swift
+//  Memorize_stanford
+//
+//  Created by Artur Campos on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_stanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
