cafb68bad29899c87c73dcb42148f617b0215ca3
Artur0campos <arturcamposcamara@gmail.com>
2024-03-13 11:38:00 -0300

    creanting the model and the view model of the project(appllaying the mvvm)

---
+++ Memorize_stanford/mascotesMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  mascotesMemoryGame.swift
+//  Memorize_stanford
+//
+//  Created by Artur Campos on 12/03/24.
+//
+
+import Foundation
---
+++ Memorize_stanford/memorizeModel.swift
@@ -0,0 +1,8 @@
+//
+//  memorizeModel.swift
+//  Memorize_stanford
+//
+//  Created by Artur Campos on 12/03/24.
+//
+
+import Foundation
